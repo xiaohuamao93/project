5d11c23a3c68018458f023e041a64bccf9b1dcff
xiaohuamao <maoxh@jiguang.cn>
2021-01-16 11:48:34 +0800

    test

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -15,6 +15,7 @@
       </div>
     </div>
     <mt-button type="primary" size="large" @click.native.enter="handleClick">登录</mt-button>
+    <input class="not-show" ref="upload" multiple  type="file" @change="attachFile" name="file" accept="image/*" capture="camera" />
     <footer>
 <!--      <p>本网站属于个人网站，仅作为个人生活使用</p>-->
       <a href="http://www.beian.miit.gov.cn" target="_blank">
